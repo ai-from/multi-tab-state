d72931c65308ae76986186aa381dc73a731e0841
Stanley Hughes <codemusic@mail.ru>
2024-02-22 04:46:25 +0300

    fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'multi-tab-state',
+    title: 'Multi-tab-state',
     htmlAttrs: {
       lang: 'en'
     },
